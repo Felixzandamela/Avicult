104b8283d0ec8d7c3697d1db0686c06e81b16ff7
Felixzandamela <81531283+Felixzandamela@users.noreply.github.com>
2024-11-13 17:36:33 +0200

    update src/pages/auth/FetchDatas.js

--- src/pages/auth/FetchDatas.js
+++ src/pages/auth/FetchDatas.js
@@ -392,26 +392,3 @@ export const FleetsDatas = () => {
   },[]);
   return datas;
 }
-
-export const currentUser = async (e) => {
-  try {
-    let user;
-    const snapUser = await dbUsers.child(e.id).once('value');
-    if (snapUser.exists()) {
-      let userData = snapUser.val();
-      if (e.avatar) {
-        const snapImg = await dbImages.child(e.id).once("value");
-        if (snapImg.exists()) {
-          userData.avatar = snapImg.val().src;
-        }
-      }
-      user = userData;
-      return user;
-    } else {
-      throw new Error('User does not exist');
-    }
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
